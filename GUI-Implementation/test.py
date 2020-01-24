9d4f301f8f077ca9f9e4df0b678221be6785a6ab
Bassam <muhammadbassam73@gmail.com>
2020-01-24 21:58:04 +0500

    Add files via upload

---
+++ GUI-Implementation/test.py
@@ -0,0 +1,25 @@
+# from tkinter import *
+# from tkinter import ttk
+# root=Tk()
+# root.geometry("1300x600")
+# root.resizable(0, 0)
+# root.title("Inverted Index")
+# window_height = 500
+# window_width = 1250
+# screen_width = root.winfo_screenwidth()
+# screen_height = root.winfo_screenheight()
+# x_cordinate = int((screen_width / 2) - (window_width / 2))
+# y_cordinate = int((screen_height / 2) - (window_height / 2))
+# root.geometry("{}x{}+{}+{}".format(window_width, window_height, x_cordinate, y_cordinate))
+# root.configure(background='powder blue')
+# canvas = Canvas(root, width=1500, height=500)
+# canvas.pack()
+# this_image = PhotoImage(file="Search.png")
+# canvas.create_image(0,0, anchor=NW, image=this_image)
+# style = ttk.Style()
+# Entry(canvas,fg="black",bg="white",width=90).place(x=360,y=360,height=40)
+# Button(canvas,fg="black",bg="white",text="GO!",height=2,width=5).place(x=910,y=359)
+# root.mainloop()
+
+x = '1,2,3,4,4'
+print(x[-11110:10000])
